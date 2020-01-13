d288caee915132fb8d525b2f3b63e5bca79f4a42
Debuchy <florentdebuchy@orange.fr>
2020-01-13 15:58:07 +0100

    ADD JS file

---
+++ index.js
@@ -0,0 +1,13 @@
+class js
+{
+    randomBomb()
+    {
+            return Math.floor(Math.random() * 6 )+1;
+    }
+    initDemineur()
+    {
+        
+    }
+}
+const me = new js();
+console.log(me.rabbitALot());
